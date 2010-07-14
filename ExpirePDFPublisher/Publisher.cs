47755aa0e5a829639289d968920ee68ee41e4b91
Ryan.Wayne.Ross.MCSE <Ryan.Wayne.Ross.MCSE@95b02b5e-9f17-508d-f5ed-6abe1301b57b>
2010-07-14 22:28:48 +0000

    Initial commit.

---
+++ ExpirePDFPublisher/Publisher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ExpirePDFPublisher
+{
+    public partial class Publisher : Form
+    {
+        public Publisher()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Publish_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListBox_Files_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void ListBox_Files_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            
+            foreach (string file in files)
+            {
+                
+                ListBox_Files.Items.Add(file);
+            }
+
+        }
+    }
+}
