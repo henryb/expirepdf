11bac7be0f09edb8d4a15d110570f34f64d0dc93
rashed2020 <rashed2020@95b02b5e-9f17-508d-f5ed-6abe1301b57b>
2010-08-09 02:48:37 +0000

    minimods + keys

--- trunk/ExpirePDFPublisher/Publisher.Designer.cs
+++ trunk/ExpirePDFPublisher/Publisher.Designer.cs
@@ -45,21 +45,22 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.ListBox_UsersComputers = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ListBox_Dates = new System.Windows.Forms.ListBox();
             this.button3 = new System.Windows.Forms.Button();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label3 = new System.Windows.Forms.Label();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
-            this.ListBox_Dates = new System.Windows.Forms.ListBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.ListBox_UsersComputers = new System.Windows.Forms.ListBox();
-            this.button6 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -234,6 +235,91 @@
             this.tabPage2.Text = "Advanced";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 225);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(258, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(392, 280);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Remove";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "User / Computer:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(392, 251);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Add";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // ListBox_UsersComputers
+            // 
+            this.ListBox_UsersComputers.FormattingEnabled = true;
+            this.ListBox_UsersComputers.Location = new System.Drawing.Point(23, 251);
+            this.ListBox_UsersComputers.Name = "ListBox_UsersComputers";
+            this.ListBox_UsersComputers.Size = new System.Drawing.Size(363, 95);
+            this.ListBox_UsersComputers.TabIndex = 17;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 185);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Password:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(392, 116);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Remove Date";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(389, 87);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Add Date";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // ListBox_Dates
+            // 
+            this.ListBox_Dates.FormattingEnabled = true;
+            this.ListBox_Dates.Location = new System.Drawing.Point(20, 87);
+            this.ListBox_Dates.Name = "ListBox_Dates";
+            this.ListBox_Dates.Size = new System.Drawing.Size(363, 95);
+            this.ListBox_Dates.TabIndex = 12;
+            // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(442, 6);
@@ -275,97 +361,22 @@
             this.label4.TabIndex = 11;
             this.label4.Text = "Expiration Date:";
             // 
-            // ListBox_Dates
+            // label7
             // 
-            this.ListBox_Dates.FormattingEnabled = true;
-            this.ListBox_Dates.Location = new System.Drawing.Point(20, 87);
-            this.ListBox_Dates.Name = "ListBox_Dates";
-            this.ListBox_Dates.Size = new System.Drawing.Size(363, 95);
-            this.ListBox_Dates.TabIndex = 12;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(389, 87);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 13;
-            this.button4.Text = "Add Date";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(392, 116);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(110, 23);
-            this.button5.TabIndex = 14;
-            this.button5.Text = "Remove Date";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(20, 185);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 13);
-            this.label5.TabIndex = 15;
-            this.label5.Text = "Password:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(87, 185);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(296, 20);
-            this.textBox1.TabIndex = 16;
-            // 
-            // ListBox_UsersComputers
-            // 
-            this.ListBox_UsersComputers.FormattingEnabled = true;
-            this.ListBox_UsersComputers.Location = new System.Drawing.Point(23, 251);
-            this.ListBox_UsersComputers.Name = "ListBox_UsersComputers";
-            this.ListBox_UsersComputers.Size = new System.Drawing.Size(363, 95);
-            this.ListBox_UsersComputers.TabIndex = 17;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(392, 251);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 18;
-            this.button6.Text = "Add";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(20, 226);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(88, 13);
-            this.label6.TabIndex = 19;
-            this.label6.Text = "User / Computer:";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(392, 280);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 23);
-            this.button7.TabIndex = 20;
-            this.button7.Text = "Remove";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(128, 225);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(258, 20);
-            this.textBox2.TabIndex = 21;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(379, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "label7";
             // 
             // Publisher
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 674);
+            this.ClientSize = new System.Drawing.Size(533, 664);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -421,6 +432,7 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
     }
 }
 
